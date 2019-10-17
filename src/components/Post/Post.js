464a22acda7f746b9be1c465f9c9118c2b2c5edc
Ssnnaaiill <newdk3025@gmail.com>
2019-10-17 14:55:30 +0900

    Feature: create comment and post component ui Todo: - implement ajax using axios - module api functions

---
+++ src/components/Post/Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { CommentList } from '../';
+import './Post.css';
+
+const Post = () => (
+  <div className="Post">
+    <h1>Title</h1>
+    <p>
+      Body
+    </p>
+    <CommentList />
+  </div>
+);
+
+export default Post;
